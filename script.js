8a61d217463da48a865f3a051e7931f034dacb4e
mandim02 <a21160@alunos.ipca.pt>
2022-05-03 20:11:21 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let APIURL = "http://localhost:3000"
+
+
+function frontPageinteractions(){
+    
+    let utenteBtn = document.querySelector(".utente-front-page")
+    let staffBtn = document.querySelector(".staff-front-page")
+
+    utenteBtn.addEventListener("mouseover", function(){
+        utenteBtn.classList.add("front-page-side-hovered");
+    });
+
+    utenteBtn.addEventListener("mouseout", function(){
+        utenteBtn.classList.remove("front-page-side-hovered");
+    } );
+
+
+    utenteBtn.addEventListener("click", function(){
+        document.querySelector(".front-page").classList.add("hidden");
+        document.querySelector(".login").classList.remove("hidden");
+        document.querySelector(".password-region").classList.add("hidden");
+        document.getElementById("login-box-header-text").textContent = "Utente";
+    })
+
+    staffBtn.addEventListener("mouseover", function(){
+        staffBtn.classList.add("front-page-side-hovered");
+    });
+    staffBtn.addEventListener("mouseout", function(){
+        staffBtn.classList.remove("front-page-side-hovered");
+    } );
+
+    staffBtn.addEventListener("click", function(){
+        document.querySelector(".front-page").classList.add("hidden");
+        document.querySelector(".login").classList.remove("hidden");
+        document.getElementById("login-box-header-text").textContent = "Staff"
+        
+    })
+}
+
+function loginFunction(){
+    if (document.getElementById("login-box-header-text").textContent === "Utente"){
+        let username = document.getElementById("login-form").username.value;
+        let request = new HTTPRequest();
+        request.get(APIURL + "/Utente")
+    }
+    if (document.getElementById("login-box-header-text").textContent === "Staff"){
+        let username = document.getElementById("login-form").username.value;
+        let password = document.getElementById("login-form").password.value;
+    }
+    
+    
+    console.log("user:" + username + "Pass:" + password);
+    return {username, password}
+}
+
+function setup(){
+    frontPageinteractions();
+}
+
+setup();
